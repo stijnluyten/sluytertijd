f4c1f5269a1b469bcb6393bdb2909d53582a0066
Stijn Luyten <stijn.luyten@cegeka.be>
2018-06-21 10:03:19 +0200

    some more angularization

--- js/albums.js
+++ js/albums.js
@@ -1,20 +1,67 @@
+albums = {
+    buiten: {
+        title: "Buiten",
+        caption: "Foto's in de buitenlucht",
+        pictures: [
+            {
+                src: "DSC_0378-1.jpg",
+                caption: "Veterknoper"
+            },
+            {
+                src: "DSC_0378-1.jpg",
+                caption: "Veterknoper"
+            },
+            {
+                src: "DSC_0378-1.jpg",
+                caption: "Veterknoper"
+            }
+        ]
+    },
+    studio: {
+        title: "Studio",
+        caption: "Foto's in de studio",
+        pictures: [
+            {
+                src: "DSC_0347-2.jpg",
+                caption: "Broers 1"
+            },
+            {
+                src: "thorsday4.jpg",
+                caption: "Thor"
+            }
+        ]
+    }
+};
+
+
 var app = angular.module('sluytertijd', []);
-app.controller('albumController', function($scope) {
+
+app.directive('sluytertijdAlbum', function () {
+    return {
+        restrict: 'E',
+        templateUrl: "partials/sluytertijdAlbum.html",
+        scope: {
+            album: '='
+        },
+        controller: 'albumController',
+        controllerAs: 'ctrl'
+    };
+});
+
+app.controller('albumController', ['$scope', function ($scope) {
     var ctrl = this;
 
-    ctrl.pictures = [
-        {
-            src: "images/DSC_0347-2.jpg",
-            caption: "Broers 1"
-        },
-        {
-            src: "images/DSC_0378-1.jpg",
-            caption: "Veterknoper"
-        }
-    ];
-    ctrl.selectedPicture = ctrl.pictures[0];
+    ctrl.selectedPicture = undefined;
+
+    ctrl.getAlbum = function () {
+        return albums[$scope.album];
+    };
+
+    ctrl.getPathToSmallPicture = function(picture) {
+        return "images/" + $scope.album + "/small/" + picture.src;
+    }
 
     ctrl.isSelected = function (picture) {
         return picture === ctrl.selectedPicture;
     }
-});
+}]);
