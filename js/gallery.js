d8597c9f5174d9d2dae01f246f7cdc9dddde64fc
Stijn Luyten <stijn.luyten@cegeka.com>
2019-06-14 13:55:17 +0200

    image gallery javascript

---
+++ js/gallery.js
@@ -0,0 +1,40 @@
+var imagesOnPage = [];
+var currentImage = undefined;
+
+$('.image-gallery .image img').each(
+    function (index, element) {
+        imagesOnPage.push(element.attributes.src.value);
+    }
+);
+
+$('.image-gallery .image').click(
+    function (event) {
+        currentImage = event.target.attributes.src.value;
+
+        $('.image-detail .image img').attr('src', currentImage);
+        $('.image-detail').show();
+    }
+);
+
+$('.image-detail .image').click(
+    function () {
+        console.log('clicked detail!');
+        $('.image-detail').hide();
+    }
+);
+
+$('.image-detail .next').click(function () {
+    var indexOfCurrentImage = imagesOnPage.indexOf(currentImage);
+    if (imagesOnPage[indexOfCurrentImage + 1]) {
+        currentImage = imagesOnPage[indexOfCurrentImage + 1];
+    }
+    $('.image-detail .image img').attr('src', currentImage);
+});
+
+$('.image-detail .previous').click(function () {
+    var indexOfCurrentImage = imagesOnPage.indexOf(currentImage);
+    if (imagesOnPage[indexOfCurrentImage - 1]) {
+        currentImage = imagesOnPage[indexOfCurrentImage - 1];
+    }
+    $('.image-detail .image img').attr('src', currentImage);
+});
