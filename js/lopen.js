940cad31455cca1ee7d60b61df3961e2c0395f09
Stijn Luyten <stijn.luyten@cegeka.com>
2020-05-22 09:57:17 +0200

    comicbook style started

--- js/lopen.js
+++ js/lopen.js
@@ -1,71 +1,341 @@
-var schedule = [
-  {action: '++', time: 2},// ++
-  {action: '00', time: 2},// OO
-  {action: '++', time: 2},// ++
-  {action: 'O', time: 1},// O
-  {action: '++', time: 2},// ++
-  {action: 'OO', time: 2},// OO
-  {action: '++', time: 2},// ++
-  {action: '00', time: 2},// 00
-  {action: '++', time: 2},// ++
-  {action: 'O', time: 1},// O
-  {action: '++', time: 2},// ++
-  {action: 'O', time: 1},// O
-  {action: '++', time: 2},// ++
-  {action: 'OO', time: 2}// OO
+var lessons = [
+  {
+    lessonTitle: 'Les 10',
+    timeInMinutes: 24,
+    // +O++OO+++OOO+++OOO+++OOO
+    steps: [
+      {action: '+', time: 1},
+      {action: 'O', time: 1},
+      {action: '++', time: 2},
+      {action: 'OO', time: 2},
+      {action: '+++', time: 3},
+      {action: 'OOO', time: 3},
+      {action: '+++', time: 3},
+      {action: 'OOO', time: 3},
+      {action: '+++', time: 3},
+      {action: 'OOO', time: 3}
+    ]
+  },
+  {
+    lessonTitle: 'Les 11',
+    timeInMinutes: 25,
+    // ++OO++O++OO++OO++O++O++OO
+    steps: [
+      {action: '++', time: 2},
+      {action: 'OO', time: 2},
+      {action: '++', time: 2},
+      {action: 'O', time: 1},
+      {action: '++', time: 2},
+      {action: 'OO', time: 2},
+      {action: '++', time: 2},
+      {action: 'OO', time: 2},
+      {action: '++', time: 2},
+      {action: 'O', time: 1},
+      {action: '++', time: 2},
+      {action: 'O', time: 1},
+      {action: '++', time: 2},
+      {action: 'OO', time: 2}
+    ]
+  },
+  {
+    lessonTitle: 'Les 12',
+    timeInMinutes: 27,
+    // +O++OO++++OOO++++OOO+++++OO
+    steps: [
+      {action: '+', time: 1},
+      {action: 'O', time: 1},
+      {action: '++', time: 2},
+      {action: 'OO', time: 2},
+      {action: '++++', time: 4},
+      {action: 'OOO', time: 3},
+      {action: '++++', time: 4},
+      {action: 'OOO', time: 3},
+      {action: '+++++', time: 5},
+      {action: 'OO', time: 2}
+    ]
+  },
+  {
+    lessonTitle: 'Les 13',
+    timeInMinutes: 30,
+    // ++O++OO+++++OOO+++++OOO+++++OO
+    steps: [
+      {action: '++', time: 2},
+      {action: 'O', time: 1},
+      {action: '++', time: 2},
+      {action: 'OO', time: 2},
+      {action: '+++++', time: 5},
+      {action: 'OOO', time: 3},
+      {action: '+++++', time: 5},
+      {action: 'OOO', time: 3},
+      {action: '+++++', time: 5},
+      {action: 'OO', time: 2}
+    ]
+  },
+  {
+    lessonTitle: 'Les 14',
+    timeInMinutes: 33,
+    steps: [
+      {action: '++', time: 2},
+      {action: 'O', time: 1},
+      {action: '+++', time: 3},
+      {action: 'OO', time: 2},
+      {action: '++++++', time: 6},
+      {action: 'OO', time: 2},
+      {action: '++++++', time: 6},
+      {action: 'OO', time: 2},
+      {action: '+++++++', time: 7},
+      {action: 'OO', time: 2}
+    ]
+  },
+  {
+    lessonTitle: 'Les 15',
+    timeInMinutes: 34,
+    // ++OO++++OO+++++OO++++++OO+++++++OO
+    steps: [
+      {action: '++', time: 2},
+      {action: 'OO', time: 2},
+      {action: '++++', time: 4},
+      {action: 'OO', time: 2},
+      {action: '+++++', time: 5},
+      {action: 'OO', time: 2},
+      {action: '++++++', time: 6},
+      {action: 'OO', time: 2},
+      {action: '+++++++', time: 7},
+      {action: 'OO', time: 2}
+    ]
+  },
+  {
+    lessonTitle: 'Les 16',
+    timeInMinutes: 30,
+    // ++O++OO+++++OOO+++++OOO+++++OO
+    steps: [
+      {action: '++', time: 2},
+      {action: 'O', time: 1},
+      {action: '++', time: 2},
+      {action: 'OO', time: 2},
+      {action: '+++++', time: 5},
+      {action: 'OOO', time: 3},
+      {action: '+++++', time: 5},
+      {action: 'OOO', time: 3},
+      {action: '+++++', time: 5},
+      {action: 'OO', time: 2}
+    ]
+  },
+  {
+    lessonTitle: 'Les 17',
+    timeInMinutes: 33,
+    steps: [
+      {action: '++', time: 2},
+      {action: 'O', time: 1},
+      {action: '+++', time: 3},
+      {action: 'OO', time: 2},
+      {action: '++++++', time: 6},
+      {action: 'OO', time: 2},
+      {action: '++++++', time: 6},
+      {action: 'OO', time: 2},
+      {action: '+++++++', time: 7},
+      {action: 'OO', time: 2}
+    ]
+  },
+  {
+    lessonTitle: 'Les 18',
+    timeInMinutes: 34,
+    // ++OO++++OO+++++OO++++++OO+++++++OO
+    steps: [
+      {action: '++', time: 2},
+      {action: 'OO', time: 2},
+      {action: '++++', time: 4},
+      {action: 'OO', time: 2},
+      {action: '+++++', time: 5},
+      {action: 'OO', time: 2},
+      {action: '++++++', time: 6},
+      {action: 'OO', time: 2},
+      {action: '+++++++', time: 7},
+      {action: 'OO', time: 2}
+    ]
+  },
+  {
+    lessonTitle: 'Les 19',
+    timeInMinutes: 32,
+    // les 19 +++++O++++++OO+++++++OO++++++++O 32 min
+    steps: [
+      {action: '+++++', time: 5},
+      {action: 'O', time: 1},
+      {action: '++++++', time: 6},
+      {action: 'OO', time: 2},
+      {action: '+++++++', time: 7},
+      {action: 'OO', time: 2},
+      {action: '++++++++', time: 8},
+      {action: 'O', time: 1}
+    ]
+
+  },
+  {
+    lessonTitle: 'Les 20',
+    timeInMinutes: 37,
+    // les 20 ++++++++O++++++++OO++++++++O++++++++O 37 min
+    steps: [
+      {action: '++++++++', time: 8},
+      {action: 'O', time: 1},
+      {action: '++++++++', time: 8},
+      {action: 'OO', time: 2},
+      {action: '++++++++', time: 8},
+      {action: 'O', time: 1},
+      {action: '++++++++', time: 8},
+      {action: 'O', time: 1}
+    ]
+  },
+  {
+    lessonTitle: 'Les 21',
+    timeInMinutes: 37,
+    // les 21 ++++++++++OO++++++++++OO++++++++++++O 37 min
+    steps: [
+      {action: '10 &#128006;', time: 10},
+      {action: '2 &#128034;', time: 2}, 
+      {action: '10 &#128006;', time: 10},
+      {action: '2 &#128034;', time: 2},
+      {action: '12 &#128006;', time: 12},
+      {action: '1 &#128034;', time: 1}
+    ]
+  },
+  {
+    lessonTitle: 'Les 22',
+    timeInMinutes: 34,
+    // les 22 +++++++++++++++OO+++++++++++++++OO 34 min
+    steps: [
+      {action: '15 &#128006;', time: 15},
+      {action: '2 &#128034;', time: 2},
+      {action: '15 &#128006;', time: 15},
+      {action: '2 &#128034;', time: 2}
+    ]
+  },
+  {
+    lessonTitle: 'Les 23',
+    timeInMinutes: 37,
+    // les 23 ++++++++++O++++++++++++O++++++++++++O 37 min
+    steps: [
+      {action: '10 &#128006;', time: 10},
+      {action: '1 &#128034;', time: 1},
+      {action: '12 &#128006;', time: 12},
+      {action: '1 &#128034;', time: 1},
+      {action: '12 &#128006;', time: 12},
+      {action: '1 &#128034;', time: 1}
+    ]
+  },
+  {
+    lessonTitle: 'Les 24',
+    timeInMinutes: 32,
+    // les 24 ++++++++++O++++++++++++++++++++O 32 min
+    steps: [
+      {action: '10 &#128006;', time: 10},
+      {action: '1 &#128034;', time: 1},
+      {action: '20 &#128006;', time: 20},
+      {action: '1 &#128034;', time: 1}
+    ]
+  },
+  {
+    lessonTitle: 'Les 25',
+    timeInMinutes: 34,
+    // les 25 +++++++++++++++OO+++++++++++++++OO 34 min
+    steps: [
+      {action: '15 &#128006;', time: 15},
+      {action: '2 &#128034;', time: 2},
+      {action: '15 &#128006;', time: 15},
+      {action: '2 &#128034;', time: 2}
+    ]
+  },
+  {
+    lessonTitle: 'Les 26',
+    timeInMinutes: 37,
+    // les 26 ++++++++++O++++++++++++O++++++++++++O 37 min
+    steps: [
+      {action: '10 &#128006;', time: 10},
+      {action: '1 &#128034;', time: 1},
+      {action: '12 &#128006;', time: 12},
+      {action: '1 &#128034;', time: 1},
+      {action: '12 &#128006;', time: 12},
+      {action: '1 &#128034;', time: 1}
+    ]
+  },
+  {
+    lessonTitle: 'Les 28',
+    timeInMinutes: 32,
+    // les 28 ++++++++++O++++++++++++++++++++O 32 min
+    steps: [
+      {action: '10 &#128006;', time: 10},
+      {action: '1 &#128034;', time: 1},
+      {action: '20 &#128006;', time: 20},
+      {action: '1 &#128034;', time: 1}
+    ]
+  },
+
+  {
+    lessonTitle: 'Les 29',
+    timeInMinutes: 30,
+    // 30min joggen met 1 of 2min wandelen naar keuze
+    steps: [
+      {action: '30min joggen met 1 of 2min wandelen naar keuze', time: 30}
+    ]
+  },
+
+  {
+    lessonTitle: 'Les 30',
+    timeInMinutes: 32,
+    // 32min joggen met 1 of 2min wandelen naar keuze
+    steps: [
+      {action: '32min joggen met 1 of 2min wandelen naar keuze', time: 32}
+    ]
+  },
+
+  {
+    lessonTitle: 'Les 31',
+    timeInMinutes: 34,
+    // Test 5 km Succes!
+    steps: [
+      {action: 'Test 5 km Succes!', time: 34}
+    ]
+  }
 ];
 
-var nextSchedule = [
-  {action: '+', time: 1},
-  {action: 'O', time: 1},
-  {action: '++', time: 2},
-  {action: 'OO', time: 2},
-  {action: '++++', time: 4},
-  {action: 'OOO', time: 3},
-  {action: '++++', time: 4},
-  {action: 'OOO', time: 3},
-  {action: '+++++', time: 5},
-  {action: 'OO', time: 2}
-];
+var selectedSchedule;
 
 var soundEffect = new Audio('audio/just-saying.mp3');
 
-var secondsInMinute = 2;
-
+var secondsInMinute = 60;
 
 function scheduleWO(index) {
   soundEffect.src = 'audio/just-saying.mp3';
   soundEffect.play();
   soundEffect.pause();
-  console.dir(schedule[index]);
+  console.dir(lessons[selectedSchedule].steps[index]);
   $('#step_' + index).addClass('busy');
-  tickMinutes(schedule[index].time, function () {
+  tickMinutes(lessons[selectedSchedule].steps[index].time, function () {
     $('#step_' + index).removeClass('busy');
     $('#step_' + index).addClass('done');
     $('.trigger').click();
   });
 }
 
-
 $(function () {
+  $('.lesson').remove();
+  lessons.forEach(function (lesson, index) {
+    $('.lessons').append(`<div id="lesson_${index}" class="lesson" onclick="javascript:chooseLesson(${index});">${lesson.lessonTitle}</div>`);
+  });
+});
+
+
+function chooseLesson(index) {
+  var lesson = lessons[index];
+  selectedSchedule = index;
+  $('.lessonTitle').text(lesson.lessonTitle);
+
+  $('.lessons').remove();
   $('.step').remove();
-  schedule.forEach(function (step, index) {
+  lesson.steps.forEach(function (step, index) {
     $('.steps').append(`<div id="step_${index}" class="step" onclick="javascript:scheduleWO(${index});">${step.action}</div>`);
   });
-  /*
-    $('.step').each(function(index, element) {
-      console.log('schedule will be: index' + JSON.stringify(schedule[index]));
-      $(element).click(function() {
-        console.dir(schedule[index]);
-        $('#step_' + index).addClass('busy');
-        tickMinutes(schedule[index].time, function() {
-          $('#step_' + index).removeClass('busy');
-          $('#step_' + index).addClass('done');
-        });
-      });
-    });
-    */
-});
+  showTime(`${lesson.timeInMinutes} min`);
+}
 
 function tickMinutes(minutes, functionAtEnd) {
   //functionAtEnd();
@@ -87,26 +357,24 @@ function beep() {
 }
 
 function tickUntil(startTime, timeToStop, functionAtEnd) {
-  console.log('tickUntil! ' + functionAtEnd);
   setTimeout(function () {
     var now = new Date().getTime();
     if (now > timeToStop) {
       showTime('Klaar');
-      console.log('beep! ' + functionAtEnd);
       beep();
       if (functionAtEnd) {
-        console.log('next function');
         functionAtEnd();
       }
     } else {
       tickUntil(startTime, timeToStop, functionAtEnd);
-      var timeToShow = (timeToStop - now) / 1000;
-      showTime(timeToShow);
+      var timeInSeconds = (timeToStop - now) / 1000;
+      timeInSeconds = Math.ceil(timeInSeconds);
+
+      var minutesPart = Math.floor(timeInSeconds / 60);
+      var secondsPart = timeInSeconds - (minutesPart * 60);
+      secondsPart = secondsPart < 10 ? `0${secondsPart}` : secondsPart;
+
+      showTime(`${minutesPart}:${secondsPart}`);
     }
   }, 100);
 }
-
-/*
-1581892251880
-1581892251890000
-*/
