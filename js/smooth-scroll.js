496548d5a8d7c6c45b6efd55ecdc5441fa3f2dff
Stijn Luyten <stijn.luyten@cegeka.com>
2020-06-04 08:09:27 +0200

    cta arrow complete :)

---
+++ js/smooth-scroll.js
@@ -0,0 +1,16 @@
+$("a").on('click', function (event) {
+
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+
+    var goTo = $(hash);
+    var scrollTo = goTo.offset().top;
+
+    $('html, body').animate({
+      scrollTop: scrollTo
+    }, 400, 'swing', function () {
+      // window.location.hash = hash;
+    });
+  }
+});
